285170fb95822d173a20d35fd093d6d50a83edd5
harunalbayrak <harunnalbayrakk278@gmail.com>
2021-03-02 17:51:01 +0300

    Add some backend features

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item-model';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  budgetItem: BudgetItem[] = new Array<BudgetItem>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItem(newItem: BudgetItem){
+    this.budgetItem.push(newItem);  
+  }
 }
